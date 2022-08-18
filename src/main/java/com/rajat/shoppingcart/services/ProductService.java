c27d1f02453387b463023a30ffc15cffb45d8b2c
Rajat Singh <singhrp.rajat98@gmail.com>
2022-08-18 16:03:55 +0530

    added bootstrap dependency and @autowired

--- src/main/java/com/rajat/shoppingcart/services/ProductService.java
+++ src/main/java/com/rajat/shoppingcart/services/ProductService.java
@@ -1,6 +1,8 @@
 package com.rajat.shoppingcart.services;
 
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,8 +16,10 @@ import java.util.Optional;
 @Transactional
 @Slf4j
 public class ProductService {
+    
     private final ProductRepo productRepository;
 
+    @Autowired
     public ProductService(ProductRepo productRepository) {
         this.productRepository = productRepository;
     }
--- src/main/java/com/rajat/shoppingcart/web/controllers/ProductController.java
+++ src/main/java/com/rajat/shoppingcart/web/controllers/ProductController.java
@@ -4,6 +4,8 @@ import com.rajat.shoppingcart.entities.Product;
 import com.rajat.shoppingcart.exceptions.ProductNotFoundException;
 import com.rajat.shoppingcart.services.ProductService;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,7 @@ public class ProductController {
  
     private final ProductService productService;
  
+    @Autowired
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
